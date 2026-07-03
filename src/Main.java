932982c0d3bcdaa0cbe2a76632ff100b01d22d3c
Bhagyashree-hiremath <bhagyashreeh65@gmail.com>
2026-07-03 08:06:32 +0530

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[]args){
+        System.out.println("================================================");
+        System.out.println("      Welcome to CodeVault     ");
+        System.out.println(" Interview Preparation Tracker ");
+        System.out.println("================================================");
+    }
+}
